0a77efadb76537c827722d25d3957f401d58b06d
GabrijelaJuricic <gabjuricic@gmail.com>
2023-02-11 17:19:52 +0100

    Backend - Create get all customers endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,10 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { default: mongoose } = require("mongoose");
+const customersRoutes = express.Router();
 const PORT = 4000;
+
+let Customers = require("./customers-model");
 
 //midlewares
 app.use(cors());
@@ -17,6 +20,19 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
+
+// get all customers
+customersRoutes.route("/").get((req, res) => {
+  Customers.find((err, customers) => {
+    if (err) {
+      res.status(500).json({ message: err.message });
+    } else {
+      res.json(customers);
+    }
+  });
+});
+
+app.use("/customers", customersRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on Port: " + PORT);

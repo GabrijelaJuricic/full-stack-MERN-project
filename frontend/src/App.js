7754500879095233a9f11e3866a61b1bb3703a62
GabrijelaJuricic <gabjuricic@gmail.com>
2023-02-11 18:10:22 +0100

    Frontend - Create basic table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,35 @@
 const App = () => {
-  return <div className=""></div>;
+  return (
+    <div className="container" style={{ width: 900 }}>
+      <table className="table table-striped" style={{ marginTop: 20 }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Lastname</th>
+            <th>Email</th>
+            <th>City</th>
+            <th>Birthdate</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{"name"}</td>
+            <td>{"lastname"}</td>
+            <td>{"email"}</td>
+            <td>{"city"}</td>
+            <td>{"birthdate"}</td>
+            <td>
+              <input
+                className="btn btn-light float-end"
+                type="button"
+                value="Details"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default App;
